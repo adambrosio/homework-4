4d6f51e7fdfc4f72c862933db8709cbf1cb5966d
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-03-28 18:08:43 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var startBtn = document.getElementById("#start-btn");
+var nextBtn = document.getElementById("#next-btn");
+
+    console.log(startBtn);
+
+
+    
+
+
+
+// Variables for buttons
+
+// Click event listener
+// Hide start button
+
+// startBtn.addEventListener("click", startGame)
+
+// function startGame() {
+//     startBtn.classList.add("hide");
+//     classList.remove("hide");
+//     showQuestion()
+
+// }
+
+// function showQuestion() {
+// }
+
