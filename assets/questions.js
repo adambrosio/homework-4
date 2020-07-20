d123e7a17257008051f6c573992b69428591d52c
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-07-20 14:11:34 -0400

    updated file structure

---
+++ assets/questions.js
@@ -0,0 +1,17 @@
+const questions = [
+    {
+        title: "An array is enclosed in: ",
+        choices: [
+            "curly brackets", "quotes", "square brackets", "parentheses"
+        ],
+        answer: "square brackets"
+    },
+    {
+        title: "The less than/or equal operator is: ",
+        choices: ["<=", ">>", "+=", ">="],
+        answer: "<="
+    },
+    {
+        
+    }
+]
---
+++ assets/script.js
@@ -0,0 +1,34 @@
+// Variables for buttons
+var startBtn = document.getElementById("start-btn");
+var nextBtn = document.getElementById("next-btn");
+var questionContainer = document.getElementById("question-container");
+var questionEl = document.getElementById("questions");
+var answerBtn = document.getElementById("answer-buttons");
+
+// Click event listeners
+startBtn.addEventListener("click", startGame);
+startBtn.addEventListener("click", startTime);
+nextBtn.addEventListener("click", )
+
+
+function startGame() {
+
+}
+
+function startTime() {
+    let time = 0;
+}
+
+
+
+
+
+// Hide start button
+
+
+
+
+
+// function showQuestion() {
+// }
+
