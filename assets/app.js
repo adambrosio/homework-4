c69db27627ca05b6a7d9a3a7e89cb462cb09eb43
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-07-20 15:33:08 -0400

    getQuestion function updated

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,59 @@
-// Variables for buttons
+// Variables for buttons and question containers
 var startBtn = document.getElementById("start-btn");
 var nextBtn = document.getElementById("next-btn");
 var questionContainer = document.getElementById("question-container");
 var questionEl = document.getElementById("questions");
-var answerBtn = document.getElementById("answer-buttons");
+var answerBtn = document.getElementById("answer");
+var potentionalAns = document.getElementById("potential");
+var realTimeQuestionIndex = 0;
+// const questions1 = questions;
+// var time = questions.length;
+
 
 // Click event listeners
 startBtn.addEventListener("click", startGame);
-startBtn.addEventListener("click", startTime);
-nextBtn.addEventListener("click", )
+// startBtn.addEventListener("click", startTime);
+nextBtn.addEventListener("click", nextQuestion);
 
+const questions = [
+    {
+        title: "An array is enclosed in: ",
+        // choices: [
+        //     "curly brackets", "quotes", "square brackets", "parentheses"
+        // ],
+        answer: "square brackets"
+    },
+    {
+        title: "The less than/or equal operator is: ",
+        // choices: ["<=", ">>", "+=", ">="],
+        answer: "<="
+    }
+];
 
+// Should start game and time be one function?
 function startGame() {
 
 }
 
-function startTime() {
-    let time = 0;
+function getQuestion() {
+ let currentQuest = questions[realTimeQuestionIndex]; // also timer
+ let questionTitle = document.getElementById("title-of-question")
+ potentionalAns.innerHTML = "";
+    questionTitle.textContent = currentQuest;
+    currentQuest.answer.forEach(function (answer, i) {
+        var answerEl = document.createElement("button");
+        answerEl.setAttribute("class", "answer");
+        answerEl.setAttribute("value", "answer");
+        answerEl.textContent = i + 1 + answer;
+        potentionalAns.appendChild(answerEl);
+    })
+    
+}
+
+console.log(questions);
+
+function nextQuestion() {
+
 }
 
 
