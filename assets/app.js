cd5b30648a30b3821242afba8d3fe8559caf729f
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-07-29 14:11:47 -0400

    questions and button choices appear on page

--- assets/app.js
+++ assets/app.js
@@ -1,68 +1,78 @@
 // Variables for buttons and question containers
-var startBtn = document.getElementById("start-btn");
-var nextBtn = document.getElementById("next-btn");
+var startBtn = document.querySelector(".start-btn");
+// var nextBtn = document.getElementById("next-btn");
 var questionContainer = document.getElementById("question-container");
 var questionEl = document.getElementById("questions");
 var answerBtn = document.getElementById("answer");
-var potentionalAns = document.getElementById("potential");
-var realTimeQuestionIndex = 0;
+var potentionalAns = document.getElementById("potential-answers");
+var timer = document.getElementById("timer");
+var time = 60;
+var score = 0;
+var realTimeIndex = 0;
 
 // Questions and answers variable set to array of objects containing questions
 const questions = [
     {
         title: "An array is enclosed in: ",
-        // choices: [
-        //     "curly braces", "quotes", "square brackets", "parentheses"
-        // ],
+        choices: [
+            "curly braces", "quotes", "square brackets", "parentheses"
+        ],
         answer: "square brackets"
     },
     {
         title: "The less than/or equal to operator is: ",
-        // choices: ["<=", ">>", "+=", ">="],
+        choices: ["<=", ">>", "+=", ">="],
         answer: "<="
     },
     {
         title: `Which of the following is a programming "language"?`,
+        choices: ["HTML", "CSS", "JavaScript", "Bootstrap"],
         answer: "JavaScript"
     }
 ];
 
-// Click event listeners for buttons
+// Click event listeners for buttons. CURRENTLY BLOCKING CONSOLE LOG WHILST UNCOMMENTED.
 startBtn.addEventListener("click", startGame);
-// startBtn.addEventListener("click", startTime);
-nextBtn.addEventListener("click", nextQuestion);
+// nextBtn.addEventListener("click", nextQuestion);
 
 
 
 // Should start game and time be one function?
-function startGame(event) {
-    event.preventDefault();
+function startGame() {
+    startBtn.setAttribute("class", "hide");
+    questionEl.removeAttribute("class");
+    getQuestion();
 }
 
 function getQuestion() {
-    let currentQuest = questions[realTimeQuestionIndex]; // also timer
+    let currentQuest = questions[realTimeIndex];
     let questionTitle = document.getElementById("title-of-question")
     potentionalAns.innerHTML = "";
-    questionTitle.textContent = currentQuest;
-    currentQuest.answer.forEach(function (answer, i) {
+    questionTitle.textContent = currentQuest.title;
+    currentQuest.choices.forEach(function (choice) {
         var answerEl = document.createElement("button");
-        answerEl.setAttribute("class", "answer");
-        answerEl.setAttribute("value", "answer");
-        answerEl.textContent = i + 1 + answer;
+        answerEl.setAttribute("class", "choice");
+        answerEl.setAttribute("value", choice);
+        answerEl.textContent = choice;
+        answerEl.onclick = nextQuestion;
         potentionalAns.appendChild(answerEl);
-        potentionalAns.append(questions[0]);
-        
     })
-    
 }
 
 // console.log questions declaration with questions and answers
-// console.log(questions);
+console.log(questions);
 
 function nextQuestion() {
-
+    if (this.value === questions[realTimeIndex].answer) {
+        console.log("correct");
+    } else {
+        console.log("incorrect");
+    }
 }
 
+function startTimer() {
+
+}
 
 
 
